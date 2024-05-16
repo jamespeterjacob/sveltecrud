5c03c40ffa34bbf261b038d39c771f03c26839ef
jamespeterjacob <jamespeterjacob@gmail.com>
2024-05-16 15:56:27 -0700

    new file:   src/lib/server/database.js 	modified:   src/routes/+layout.svelte 	modified:   src/routes/+page.server.js 	modified:   src/routes/+page.svelte 	new file:   src/routes/remove.svg 	new file:   src/routes/roll/+page.svelte 	new file:   src/routes/roll/+server.js

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,53 @@
+<script>
+	import { page, navigating, updated } from '$app/stores';
+</script>
+
 <nav>
-	<a href="/">home</a>
-	<a href="/blog">blog</a>
+	<a href="/" aria-current={$page.url.pathname === '/'}>
+		home
+	</a>
+
+	<a href="/blog" aria-current={$page.url.pathname === '/blog'}>
+		blog
+	</a>
+
+	<!-- {#if $navigating}
+		navigating to {$navigating.to.url.pathname}
+	{/if} -->
 </nav>
 
 <slot />
+
+{#if $updated}
+	<div class="toast">
+		<p>
+			A new version of the app is available
+
+			<button on:click={() => location.reload()}>
+				reload the page
+			</button>
+		</p>
+	</div>
+{/if}
+
+<style>
+	.toast {
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		padding: 1rem;
+		display: flex;
+		justify-content: center;
+		gap: 1rem;
+	}
+
+	.toast p {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		background: var(--bg-2);
+		padding: 0.5rem 0.5rem 0.5rem 1rem;
+		border-radius: 4px;
+	}
+</style>
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,37 @@
-export function load({ cookies }) {
-	const visited = cookies.get('visited');
+import { fail } from '@sveltejs/kit';
+import * as db from '$lib/server/database.js';
 
-	cookies.set('visited', 'true', { path: '/' });
+export function load({ cookies }) {
+	let id = cookies.get('userid');
+
+	if (!id) {
+		id = crypto.randomUUID();
+		cookies.set('userid', id, { path: '/' });
+	}
 
 	return {
-		visited: visited === 'true'
+		todos: db.getTodos(id)
 	};
 }
+
+export const actions = {
+	create: async ({ cookies, request }) => {
+		await new Promise((fulfil) => setTimeout(fulfil, 1000));
+		const data = await request.formData();
+
+		try {
+			db.createTodo(cookies.get('userid'), data.get('description'));
+		} catch (error) {
+			return fail(422, {
+				description: data.get('description'),
+				error: error.message
+			});
+		}
+	},
+
+	delete: async ({ cookies, request }) => {
+		await new Promise((fulfil) => setTimeout(fulfil, 1000));
+		const data = await request.formData();
+		db.deleteTodo(cookies.get('userid'), data.get('id'));
+	}
+};
